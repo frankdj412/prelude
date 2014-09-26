0776cc20bd328579bc12535695721a27ea2b39f1
frankdj412 <frankdj412@gmail.com>
2014-09-26 18:32:56 +0800

    1. Add snippets submodule. 2. Modify yasnippet setting: Popup using helm.

--- personal/personal.el
+++ personal/personal.el
@@ -14,11 +14,13 @@
 
 
 ;; Theme Setting
-;; (disable-theme 'zenburn)
+(disable-theme 'zenburn)
+(load-theme 'spolsky t)
 ;; (load-theme 'solarized-dark t)
 
 ;; Company Mode Setting
-(setq company-idle-delay 0)
+;; (setq company-idle-delay 0)
+;; (setq company-minimum-prefix-length 0)
 
 ;; Set the start frame
 ;; (when window-system
@@ -27,3 +29,42 @@
 
 (add-to-list 'default-frame-alist '(width . 90))
 (add-to-list 'default-frame-alist '(height . 48))
+
+;;; Configure the YaSnippet
+(add-to-list 'load-path "~/.emacs.d/snippets")
+(require 'yasnippet)
+
+;; Reload all the snippets at startup
+(yas/reload-all)
+(add-hook 'prog-mode-hook
+          '(lambda ()
+             (yas-minor-mode)))
+
+;; Use Helm to select the snippet
+(defun shk-yas/helm-prompt (prompt choices &optional display-fn)
+    "Use helm to select a snippet. Put this into `yas/prompt-functions.'"
+    (interactive)
+    (setq display-fn (or display-fn 'identity))
+    (if (require 'helm-config)
+        (let (tmpsource cands result rmap)
+          (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
+          (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x)) choices))
+          (setq tmpsource
+                (list
+                 (cons 'name prompt)
+                 (cons 'candidates cands)
+                 '(action . (("Expand" . (lambda (selection) selection))))
+                 ))
+          (setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
+          (if (null result)
+              (signal 'quit "user quit!")
+            (cdr (assoc result rmap))))
+      nil))
+(add-hook 'yas/prompt-functions 'shk-yas/helm-prompt)
+
+
+
+;;; Configure Company-mode
+;; Trigger key
+(global-set-key (kbd "S-b") 'company-complete)
+;;;
