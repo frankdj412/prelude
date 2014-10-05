8f26ef89941dacec8a5a8d1660e511a8d33ed415
frankdj412 <frankdj412@gmail.com>
2014-10-06 01:03:01 +0800

    Chagne file name.

---
+++ personal/yasnippet.el
@@ -0,0 +1,39 @@
+(prelude-require-package 'yasnippet)
+
+;; Add snippet path
+;; Warning:
+;; No need to load snippet again
+;; (add-to-list 'load-path "~/.emacs.d/snippets")
+
+(require 'yasnippet)
+
+;; Reload all the snippets at startup
+(yas/reload-all)
+(add-hook 'prog-mode-hook
+          '(lambda ()
+             (yas-minor-mode)))
+
+;; Use Helm to select the snippet
+(defun shk-yas/helm-prompt (prompt choices &optional display-fn)
+    "Use helm to select a snippet. Put this into `yas/prompt-functions.'"
+    (interactive)
+    (setq display-fn (or display-fn 'identity))
+    (if (require 'helm-config)
+        (let (tmpsource cands result rmap)
+          (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
+          (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x)) choices))
+          (setq tmpsource
+                (list
+                 (cons 'name prompt)
+                 (cons 'candidates cands)
+                 '(action . (("Expand" . (lambda (selection) selection))))
+                 ))
+          (setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
+          (if (null result)
+              (signal 'quit "user quit!")
+            (cdr (assoc result rmap))))
+      nil))
+
+(add-hook 'yas/prompt-functions 'shk-yas/helm-prompt)
+
+;; (add-to-list 'company-backends '(company-clang company-yasnippet))
