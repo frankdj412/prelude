6cab4841aaf91c7ba8019e9c26f10da93c6ffe5c
frankdj412 <frankdj412@gmail.com>
2014-11-04 22:53:13 +0800

    Change the font to MonacoB

--- personal/init-theme.el
+++ personal/init-theme.el
@@ -10,14 +10,14 @@
 ;;; -----------------------------
 ;; For emacs daemon, default font has to be set in this way
 ;; Source: http://stackoverflow.com/questions/3984730/emacs-gui-with-emacs-daemon-not-loading-fonts-correctly"
-(setq default-frame-alist '((font . "-apple-Monaco-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")))
+(setq default-frame-alist '((font . "-*-monacob-normal-normal-normal-*-12-*-*-*-p-0-iso10646-1")))
 
 ;; Generally this works except daemon mode.
 ;; (set-default-font "-apple-Monaco-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
 
 ;;; Set line spacing
 ;;; WARNING: This may cause the company mode crash
-(setq-default line-spacing 2)
+(setq-default line-spacing 1)
 
 
 ;;; Set the start frame
