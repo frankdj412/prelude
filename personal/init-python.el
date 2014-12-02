854cb0acb262a95351da58ad96fc027c2666e71b
frankdj412 <frankdj412@gmail.com>
2014-12-02 17:05:14 +0800

    Add python config file: init-python.el

---
+++ personal/init-python.el
@@ -0,0 +1,10 @@
+;;; package -- Summary
+
+;;; Commentary:
+;; After using emacs:jedi and elpy, this is the best backend for python environement.
+
+;;; Code:
+(elpy-enable)
+
+
+;;; init-python.el ends here
