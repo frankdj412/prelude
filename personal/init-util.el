0ed2b3e394d6d41481118551c3e4a80e27319d9a
frankdj412 <frankdj412@gmail.com>
2014-12-17 17:20:53 +0800

    Support zshell

--- personal/init-util.el
+++ personal/init-util.el
@@ -5,13 +5,13 @@
 
 ;;; Code:
 (prelude-require-package 'names)
+(prelude-require-package 'multi-term)
 (require 'names-dev)
-
+(require 'multi-term)
 
 (define-namespace dj/
 
-:autoload
-;; open cheat sheet in evernote
+;; Open cheat sheet in evernote
 (defun open-emacs-cheatsheet (arg)
   (interactive "P")
   (let* ((filename "https://www.evernote.com/shard/s257/nl/34264237/6f524890-22a8-4439-9598-778f86a1d180/")
@@ -22,6 +22,16 @@
                       (read-shell-command "Open current file with: ")
                     open)))
     (start-process "open-emacs-cheatsheet" nil program filename))))
+
+
+;; Zshell setting
+(setq multi-term-program "/bin/zsh")
+(defun term-send-tab ()
+  "Send tab in term mode."
+  (interactive)
+  (term-send-raw-string "\t"))
+
+(add-to-list 'term-bind-key-alist '("<tab>" . term-send-tab))
 
 
 ;;; init-util.el ends here
