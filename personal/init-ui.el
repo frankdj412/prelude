3e44bc44906d006446b1511447e562bc19a1e6ff
frankdj412 <frankdj412@gmail.com>
2014-12-23 22:04:22 +0800

    Set fringe width and change 'diff-hl' symbol

--- personal/init-ui.el
+++ personal/init-ui.el
@@ -16,7 +16,6 @@
 (require 'powerline)
 (require 'powerline-evil)
 
-;; Font:
 ;; The codes are copied from spacemacs configuration
 (defun dj/set-font (font size &optional options)
   (let* ((fontstr (if options
@@ -25,30 +24,36 @@
     (add-to-list 'default-frame-alist (cons 'font fontstr))
     (set-frame-font fontstr)))
 
-(dj/set-font "Source Code Pro for Powerline" 13 "light")
+;; Font:
+(dj/set-font "Source Code Pro for Powerline" 14)
 (setq-default line-spacing 2)
-(setq-default left-fringe-width 8)
+(set-fringe-mode 8)
+;; (setq-default left-fringe-width 8)
+(setq diff-hl-fringe-bmp-function 'diff-hl-fringe-bmp-from-type)
 ;; (set-face-attribute 'region nil :foreground "white" :background "#Aa7941")
 
-(add-to-list 'default-frame-alist '(width . 83))
+(add-to-list 'default-frame-alist '(width . 86))
 (add-to-list 'default-frame-alist '(height . 39))
 
 (global-set-key (kbd "C-+") 'zoom-in)
 (global-set-key (kbd "C--") 'zoom-out)
 
 ;; Theme:
-(disable-theme 'zenburn)
+;; (disable-theme 'zenburn)
+;; (load-theme 'solarized-dark)
+;; (setq prelude-theme 'solarized-dark)
 (powerline-evil-vim-color-theme)
 
 
 (custom-set-faces
  '(font-lock-builtin-face ((t (:inherit font-lock-type-face :foreground nil))))
  '(powerline-evil-normal-face ((t (:inherit powerline-evil-base-face :background "chartreuse4"))))
- '(org-level-1 ((t (:height 1))))
- '(org-level-2 ((t (:height 1))))
- '(org-level-3 ((t (:height 1))))
- '(org-level-4 ((t (:height 1))))
- '(org-quote ((t (:foreground "SkyBlue3" :slant italic))) t))
+ ;; '(org-level-1 ((t (:height 1))))
+ ;; '(org-level-2 ((t (:height 1))))
+ ;; '(org-level-3 ((t (:height 1))))
+ ;; '(org-level-4 ((t (:height 1))))
+ '(org-quote ((t (:foreground "SkyBlue3"))) t))
+ ;; '(org-code ((t (:foreground "#b58900")))))
 
 
 ;;; init-gui.el ends here
