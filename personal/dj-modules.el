3b2b847b63394b7be6e564470ad47bbc42698615
frankdj412 <frankdj412@gmail.com>
2015-01-29 22:36:04 +0800

    Seperate CLI and GUI settings.

---
+++ personal/dj-modules.el
@@ -0,0 +1,12 @@
+;;; package -- Summary
+
+;;; Commentary:
+;; Selectively include packages according to the system
+
+;;; Code:
+(add-to-list 'load-path "~/.emacs.d/personal/dj-modules/")
+
+;;(require 'dj-cli)
+(require 'dj-gui)
+
+;;; dj-modules.el ends here
